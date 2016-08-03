a515393a129a1b09e34e286e6d3397d988cdc01c
Koby Samuel <ksamuel890@gmail.com>
2016-08-03 19:21:07 -0400

    committed

---
+++ BackgroundDownload/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BackgroundDownload
+//
+//  Created by Koby Samuel on 12/1/15.
+//  Copyright © 2015 Koby Samuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	@IBOutlet weak var backgroundImage: UIImageView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
